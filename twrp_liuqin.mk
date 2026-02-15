48cde8d7a2d93faedddb6100d8f4559035ae4071
loocul <loocul@163.com>
2026-02-15 23:53:34 +0800

    Update

--- twrp_liuqin.mk
+++ twrp_liuqin.mk
@@ -15,9 +15,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 # Enable virtual A/B OTA
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
 # Configure emulated_storage.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
